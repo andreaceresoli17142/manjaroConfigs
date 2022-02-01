d0dca9b29054ac7154a0bfcd8732810c0dfb0acd
andreaceresoli17142 <56073866+andreaceresoli17142@users.noreply.github.com>
2022-02-01 14:25:16 +0100

    automated package detection and installation

--- install_script.sh
+++ install_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sudo pacman -Syu --noconfirm i3-gaps libev xcb-util-cursor xcb-util-xrm yajl i3status ttf-dejavu xf86-video-vesa xorg-bdftopcf xorg-docs xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts-alias-100dpi xorg-fonts-alias-75dpi xorg-fonts-encodings xorg-font-util xorg-iceauth xorg-mkfontscale xorg-server-common xorg-setxkbmap xorg-xkbcomp libutempter luit xbitmaps xorg-server xorg-server-devel xorg-server-xephyr xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-smproxy xorg-util-macros xorg-x11perf xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xmodmap xorg-xpr xorg-xprop xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwininfo xorg-xwud xorg-xclock xorg-xeyes xterm libxdg-basedir rofi http-parser libgit2 starship jsoncpp libmpdclient polybar libconfig picom kitty kitty-terminfo feh neofetch vivaldi cmatrix htop c-ares minizip re2 snappy code electron13 ripgrep pulsemixer ttf-fira-code gtk2 gtkmm python python-cachecontrol python-colorama python-contextlib2 python-distlib python-html5lib python-msgpack python-pep517 python-pip python-progress python-pyopenssl python-resolvelib python-retrying python-toml python-tomli python-webencodings gucharmap i3lock xautolock aria2 xapian-core zeitgeist 
+sudo 
+
+pacman -Syu
+
+pacman -S $(cat pkgs.txt | cut -d' ' -f1)
+
 mv -f ~/manjaroConfigs/.bashrc ~/.
 #old way, needs updating
   #mkdir ~/Pictures/wallpapers
@@ -11,7 +16,6 @@ chsh -s /bin/bash
 cd ~/.config/polybar/scripts
 git clone https://github.com/andreaceresoli17142/polybarWeatherModule ./weather
 
-cd
 git clone https://github.com/andreaceresoli17142/bashScripts.git ~/.scripts
 
 sudo rm -r ~/manjaroConfigs
